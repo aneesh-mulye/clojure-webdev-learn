85e98723209a4aab100e97769137b27cecf49ea7
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2024-03-19 00:31:16 +0530

    Initial commit

---
+++ src/webdev/core.clj
@@ -0,0 +1,30 @@
+(ns webdev.core
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
+
+(defn greet [req]
+  {:status 200
+   :body "Hello, Duniya!"
+   :headers {}})
+
+(defn part [req]
+  {:status 200
+   :body "Goodbye, cruel world!"
+   :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (GET "/goodbye" [] part)
+  (not-found "Page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty
+    greet
+    {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app)
+                   {:port (Integer. port)}))
